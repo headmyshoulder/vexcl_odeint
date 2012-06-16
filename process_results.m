cf07040d5fb5b7fd42deb96855815f7e47147f32
Denis Demidov <ddemidov@ksu.ru>
2012-06-16 17:05:14 +0400

    plot cpu results

--- process_results.m
+++ process_results.m
@@ -7,6 +7,9 @@ v2 = load('vexcl_2gpu.dat');
 v3 = load('vexcl_3gpu.dat');
 n = unique(t(:,1))';
 
+vc = load('vexcl_cpu.dat');
+m = unique(vc(:,1))';
+
 tavg  = [];
 v1avg = [];
 v2avg = [];
@@ -28,27 +31,39 @@ for i = n
     v3avg = [v3avg time];
 end
 
+vcavg = [];
+
+for i = m
+    I = find(vc(:,1) == i);
+
+    time = sum(vc(I,2)) / length(I);
+    vcavg = [vcavg time];
+end
+
 figure(1)
 set(gca, 'FontSize', 18)
 
-plot(n, tavg, 'ko-', ...
+loglog(n, tavg, 'ko-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
 hold on
 
-plot(n, v1avg, 'ro-', ...
+loglog(n, v1avg, 'ro-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
-plot(n, v2avg, 'bo-', ...
+loglog(n, v2avg, 'bo-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
-plot(n, v3avg, 'go-', ...
+loglog(n, v3avg, 'go-', ...
+		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+
+loglog(m, vcavg, 'md-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
 xlabel('N');
 ylabel('T (sec)');
 
-legend('thrust', 'vexcl 1gpu', 'vexcl 2gpu', 'vexcl 3gpu', 'location', 'northwest');
+legend('thrust', 'vexcl 1gpu', 'vexcl 2gpu', 'vexcl 3gpu', 'vexcl cpu', 'location', 'northwest');
 legend boxoff
 
 print('-depsc', 'abs.eps');
@@ -56,18 +71,18 @@ print('-depsc', 'abs.eps');
 figure(2)
 set(gca, 'FontSize', 18)
 
-plot(n, v1avg ./ tavg, 'ko-', ...
+loglog(n, v1avg ./ tavg, 'ko-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
 hold on
 
-plot(n, v2avg ./ tavg, 'bo-', ...
+loglog(n, v2avg ./ tavg, 'bo-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
-plot(n, v3avg ./ tavg, 'go-', ...
+loglog(n, v3avg ./ tavg, 'go-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
-plot(n, ones(size(n)), 'k:');
+loglog(n, ones(size(n)), 'k:');
 
 legend('1gpu', '2gpu', '3gpu');
 

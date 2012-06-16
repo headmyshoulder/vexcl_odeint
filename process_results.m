43216218e00ff7a62b6f6bf8cdd99e45dd21166b
Denis Demidov <ddemidov@ksu.ru>
2012-06-16 12:11:25 +0400

    multigpu tests

--- process_results.m
+++ process_results.m
@@ -1,9 +1,13 @@
-t = load('thrust.dat');
-v = load('vexcl.dat');
+t  = load('thrust.dat');
+v1 = load('vexcl_1gpu.dat');
+v2 = load('vexcl_2gpu.dat');
+v3 = load('vexcl_3gpu.dat');
 n = unique(t(:,1))';
 
-tavg = [];
-vavg = [];
+tavg  = [];
+v1avg = [];
+v2avg = [];
+v3avg = [];
 
 for i = n
     I = find(t(:,1) == i);
@@ -11,8 +15,14 @@ for i = n
     time = sum(t(I,2)) / length(I);
     tavg = [tavg time];
 
-    time = sum(v(I,2)) / length(I);
-    vavg = [vavg time];
+    time = sum(v1(I,2)) / length(I);
+    v1avg = [v1avg time];
+
+    time = sum(v2(I,2)) / length(I);
+    v2avg = [v2avg time];
+
+    time = sum(v3(I,2)) / length(I);
+    v3avg = [v3avg time];
 end
 
 figure(1)
@@ -23,13 +33,19 @@ plot(n, tavg, 'ko-', ...
 
 hold on
 
-plot(n, vavg, 'ro-', ...
+plot(n, v1avg, 'ro-', ...
+		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+
+plot(n, v2avg, 'ro-', ...
+		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+
+plot(n, v3avg, 'ro-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
 
 xlabel('N');
 ylabel('T (sec)');
 
-legend('thrust', 'vexcl', 'location', 'northwest');
+legend('thrust', 'vexcl 1gpu', 'vexcl 2gpu', 'vexcl 3gpu', 'location', 'northwest');
 legend boxoff
 
 print('-depsc', 'abs.eps');
@@ -37,8 +53,18 @@ print('-depsc', 'abs.eps');
 figure(2)
 set(gca, 'FontSize', 18)
 
-plot(n, vavg ./ tavg, 'ko-', ...
+plot(n, v1avg ./ tavg, 'ko-', ...
 		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+
+hold on
+
+plot(n, v2avg ./ tavg, 'ko-', ...
+		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+
+plot(n, v3avg ./ tavg, 'ko-', ...
+		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+
+legend('1gpu', '2gpu', '3gpu');
 
 xlabel('N');
 ylabel('T(vexcl) / T(thrust)');
